855778ad078518338528e8f7f8dd91369e91d874
DevGibby <71801512+DevGibby@users.noreply.github.com>
2021-12-23 09:15:47 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -36,6 +36,6 @@ router.post(`/${process.env.NODE_ENV_ADD_CREATOR_URL}`, createCreator);
 router.delete(`/${process.env.NODE_ENV_DELETE_POST_URL}/:postId`, deletePost);
 router.post(`/${process.env.NODE_ENV_DELETE_COMMENT_URL}/:postId/:commentId`, deleteComment);
 router.post(`/${process.env.NODE_ENV_REMOVE_LIKE_URL}/:postId`, removeLike);
-router.post(`/${process.env.NODE_ENV_DELETE_ACCOUNT_URL}/:postId`, deleteAccount);
+router.post(`/${process.env.NODE_ENV_DELETE_ACCOUNT_URL}`, deleteAccount);
 
 export default router;
